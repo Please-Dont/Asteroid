6833edbb2e29432e5d3950e86bef01f92a2357de
Please_Dont <liam-boulton@outlook.com>
2025-09-03 15:05:44 +1000

    Player and Player movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
@@ -7,14 +8,23 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
+    Player.containers = (updatable_group, drawable_group)
+
+    player1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
     
         screen.fill(color=(0, 0, 0))
+        updatable_group.update(dt)
+        for sprite in drawable_group:
+            sprite.draw(screen)
         pygame.display.flip()
-        
+
         dt = clock.tick(60) / 1000
 
 
